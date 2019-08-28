e4822b14f23e76e36c450e89aadf40976026be1f
McMullen <mabmcmul@iu.edu>
2019-08-28 19:51:39 -0400

    Completes the assignment

--- main.py
+++ main.py
@@ -5,4 +5,11 @@ import utils
 utils.check_version((3,7))
 utils.clear()
 
-print('Hello, Class!')
+print('Hello, my name is Maddie McMullen.')
+print('My favorite game is life. lol not the boardgame (although that one is great). Honestly, I love Sims and have played it since I was 12.')
+print('I am concerned that I will either fall behind or finish things too early and be bored. Basically I am concerned about the pace of the class.')
+print('I am excited to go home and eat dinner.')
+print('11980897')
+print('GitHub profile: https://github.com/maddiemcm')
+
+print('"https://tenor.com/view/link-thumbs-up-approve-like-anime-gif-11291500"')
